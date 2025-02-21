e58e75dcc1df21fe206c6bd6200ccd73d6da3984
Panos Georgakopoulos <panos@skroutz.gr>
2025-02-21 11:01:18 +0200

    removed callback params id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,8 +60,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[)
-    end
+      @user = User.find(params
+    endgi
 
     # Only allow a list of trusted parameters through.
     def user_params
