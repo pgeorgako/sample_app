8b51c1945add792998769ab96905cf019e38e428
Panos Georgakopoulos <panos@skroutz.gr>
2025-02-27 14:33:02 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home" # Defines the root path route ("/")
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,6 +13,5 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
 end
